545d292ef8a639e516deefd230bf5ea3f0fe361c
tero nurmiluoto <lenonn@gmail.com>
2016-02-26 11:43:07 +0200

    my thinkpad thinks I want to use these keys for forward/back in my web browser. I do not.

--- xkb_stuff.sh
+++ xkb_stuff.sh
@@ -2,3 +2,5 @@ setxkbmap -option keypad:pointerkeys
 setxkbmap -layout 'us,fi'
 setxkbmap -option 'grp:caps_toggle'
 xmodmap -e 'keycode 94 = 0x60 asciitilde'
+xmodmap -e 'keycode 166 = Prior'
+xmodmap -e 'keycode 167 = Next'
