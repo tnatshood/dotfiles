5f10a018907d9851a8b5a320d5cf154d87428657
tero <none>
2022-07-13 10:57:42 +0300

    swapcaps to xkbdstuff

--- xkb_stuff.sh
+++ xkb_stuff.sh
@@ -1,6 +1,8 @@
-setxkbmap -option keypad:pointerkeys
-setxkbmap -layout 'us,fi'
-setxkbmap -option 'grp:caps_toggle'
-xmodmap -e 'keycode 94 = 0x60 asciitilde'
-xmodmap -e 'keycode 166 = Prior'
-xmodmap -e 'keycode 167 = Next'
+setxkbmap -option "caps:swapescape"
+
+#setxkbmap -option keypad:pointerkeys
+#setxkbmap -layout 'us,fi'
+#setxkbmap -option 'grp:caps_toggle'
+#xmodmap -e 'keycode 94 = 0x60 asciitilde'
+#xmodmap -e 'keycode 166 = Prior'
+#xmodmap -e 'keycode 167 = Next'
